fddcea1697f3ee7e6ff6d8a5b7f2341209d21681
gui <guilherme.moreno@ezycollect.com.au>
2022-06-25 02:44:46 -0300

    alou

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return <div className="hello">asasasaa</div>;
+    return <div className="hello" onClick={() => console.log('alouuu')}>asasasaa</div>;
 };
 
 export default App;
