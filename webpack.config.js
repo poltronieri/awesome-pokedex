c392c0e54d276518737768fd5250ddd313ce704f
gui <guilherme.moreno@ezycollect.com.au>
2022-06-26 03:58:06 -0300

    fix router

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,57 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
-  const isProduction = env === "production";
-  return {
-    mode: isProduction ? "production" : "development",
-    entry: path.resolve(__dirname, "src"),
-    devtool: isProduction ? "" : "source-map",
-    devServer: {
-      static: path.join(__dirname, "dist"),
-      hot: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        favicon: "./src/assets/img/icon.png",
-        filename: "index.html",
-      }),
-    ],
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".json", ".css", ".scss", ".ts", ".tsx"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
+    const isProduction = env === 'production';
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: path.resolve(__dirname, 'src'),
+        devtool: isProduction ? '' : 'source-map',
+        devServer: {
+            static: path.join(__dirname, 'dist'),
+            historyApiFallback: true,
+            hot: true,
         },
-        // {
-        //   test: /\.css$/i,
-        //   use: ["style-loader", "css-loader"],
-        // },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                favicon: './src/assets/img/icon.png',
+                filename: 'index.html',
+            }),
+        ],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+            clean: true,
         },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: "asset/resource",
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.css', '.scss', '.ts', '.tsx'],
         },
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                // {
+                //   test: /\.css$/i,
+                //   use: ["style-loader", "css-loader"],
+                // },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+            ],
         },
-      ],
-    },
-  };
+    };
 };
